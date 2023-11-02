87fe648fd5c1f4fef60dce44a648f40a29769f41
Leonardo <leonardo38361@gmail.com>
2023-11-02 17:47:41 -0300

    servir pages

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 
 //Middlewares
 //Servir contenido estático
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-	res.sendfile(__dirname, 'public/index.html');
+app.get('/elements', (req, res) => {
+	res.sendFile(__dirname + '/public/elements.html');
 });
 
-app.listen(8080);
+app.get('/generic', (req, res) => {
+	res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
