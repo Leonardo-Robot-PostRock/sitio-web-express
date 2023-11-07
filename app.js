c4168df4799ab304bd8a3e860f0b136ff82a4446
Leonardo <leonardo38361@gmail.com>
2023-11-07 20:21:53 -0300

    se renderiza el home y se mueven todos los html a la carpeta template

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//TODO: require('hbs')
+app.set('view engine', 'hbs');
+
 //Middlewares
 //Servir contenido estático
 app.use(express.static('public'));
 
-app.get('/elements', (req, res) => {
-	res.sendFile(__dirname + '/public/elements.html');
+app.get('/', (req, res) => {
+	res.render('home');
 });
 
 app.get('/generic', (req, res) => {
 	res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+	res.sendFile(__dirname + '/public/elements.html');
 });
 
 app.get('*', (req, res) => {
